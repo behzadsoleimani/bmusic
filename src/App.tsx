abc6f90fc23c3552b9610b51fa78e286450b9802
Behzad <b.soleimani@rismun.com>
2022-08-23 10:51:39 +0430

    implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/Home";
+import Navbar from "./components/NavBar";
+import Player from "./components/Player";
+import { AppStateValue } from "./context/state";
+import SpinStretch from "react-cssfx-loading/lib/SpinStretch";
+import client from "./utils/spotify-client";
+
+enum LoadingStates {
+  loading,
+  finished,
+  error,
 }
 
-export default App;
+export default function App() {
+  const [loadingState, setLoadingState] = useState(LoadingStates.loading);
+
+  const { playerId } = AppStateValue();
+
+  useEffect(() => {
+    localStorage.setItem("bmusic-playing", playerId);
+  }, [playerId]);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  useEffect(() => {
+    fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${btoa(
+          `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}:${
+            process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
+          }`
+        )}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: "grant_type=client_credentials",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.access_token) {
+          setLoadingState(LoadingStates.finished);
+          client.setAccessToken(data.access_token);
+        } else setLoadingState(LoadingStates.error);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoadingState(LoadingStates.error);
+      });
+  }, []);
+
+  if (loadingState === LoadingStates.loading)
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <SpinStretch />
+      </div>
+    );
+
+  if (loadingState === LoadingStates.error)
+    return <div>Something went wrong</div>;
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="min-h-[calc(100vh-144px)]">
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </div>
+
+      {!!playerId && <Player key={playerId} />}
+    </>
+  );
+}
